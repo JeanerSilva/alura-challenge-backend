124798c0bf8a62ab9954916a54534f45b0b1fcb6
Jeaner Silva <jeaner.silva@abingov.onmicrosoft.com>
2022-01-19 08:29:41 -0300

    Adicionado projeto para filtro

--- src/pages/api/receitas/index.ts
+++ src/pages/api/receitas/index.ts
@@ -1,6 +1,7 @@
 import Receita from "../models/Receita";
 import dbConnect from "../config";
 import { NextApiRequest, NextApiResponse } from "next";
+import assert from "assert";
 
 dbConnect();
 
@@ -8,35 +9,41 @@ function ehPost(method: String) {
   return method === "POST";
 }
 
-function obtemDatasLimite (data:Date){
-  const dataDate = new Date(data)
-  const d = new Date(dataDate.getFullYear(), dataDate.getMonth() + 1, 0);
-  const dataInicial = `${dataDate.getFullYear()}-${dataDate.getMonth()+1}-01`
-  const ultimoDia = d.getDate()
-  const dataFinal = `${dataDate.getFullYear()}-${dataDate.getMonth()+1}-${ultimoDia}`
-  return {dataInicial, dataFinal}
-}
+async function naoExisteMesmaReceitaNoMesmoMes(
+  res: NextApiResponse,
+  descricao: String,
+  data: Date
+): Promise<boolean> {
 
-async function naoExisteMesmaReceitaNoMesmoMes(res: NextApiResponse, descricao: String, data: Date):Promise<boolean> {
-  const {dataInicial, dataFinal } = obtemDatasLimite(data)
-  
+  data = new Date(data);
+  const agg = [
+    {
+      $project: {
+        year: {
+          $year: "$data",
+        },
+        month: {
+          $month: "$data",
+        },
+        descricao: 1,
+      },
+    },
+    {
+      $match: {
+        year: data.getFullYear(),
+        month: data.getMonth()+1,
+        descricao: descricao,
+      },
+    },
+  ];
+
   try {
-    const receitas = await Receita.find({
-      descricao,
-      data: {
-        $gte:new Date(dataInicial),
-        $lte:new Date(dataFinal),
-    }
-    });
-    
-    return receitas.length > 0 ? false : true
-
+    const receitas = await Receita.aggregate(agg);
+    return receitas.length > 0 ? false : true;
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, error });
   }
-
-
 }
 
 export default async function handler(
@@ -52,17 +59,14 @@ export default async function handler(
       if (!descricao && !valor && !data) {
         throw "Falta dados para a inserção da receita";
       } else {
-
         if (await naoExisteMesmaReceitaNoMesmoMes(res, descricao, data)) {
           const receita = await Receita.create({ descricao, valor, data });
           res.status(201).json({ success: true, data: receita });
         } else {
-          res
-            .status(500)
-            .json({
-              success: false,
-              error: "Já existe a mesma receita neste mês",
-            });
+          res.status(500).json({
+            success: false,
+            error: "Já existe a mesma receita neste mês",
+          });
         }
       }
     } catch (error) {
